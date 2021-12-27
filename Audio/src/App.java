52bffa313894d750432aca0f91c0788b0661e09f
Miguel Fung <mjfung1@gmail.com>
2021-12-27 02:59:20 -0500

    audio in java

---
+++ Audio/src/App.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.util.Scanner;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        File file = new File("batmobile_x.wav");
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
+
+        String response = "";
+        
+
+        while (!response.equals("Q")) {
+            System.out.println("P = play, S = stop, R = reset, Q = quit");
+            System.out.print("Enter your choice: ");
+
+            response = scanner.next();
+            response = response.toUpperCase();
+
+            switch(response) {
+                case "P": clip.start();
+                break;
+                case "S": clip.stop();
+                break;
+                case "R": clip.setMicrosecondPosition(0);
+                break;
+                case "Q": clip.close();
+                break;
+                default: System.out.println("not a valid response");
+            }
+        }
+
+        scanner.close();
+    }
+}
